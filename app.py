8d8385f0e11449bdefe2c975eeb3934fc3d959be
bruski <bruski@qq.com>
2020-02-10 01:10:34 +0800

    feat: add app.py to be entry

---
+++ app.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+    @author: Bruski Wang
+    @contact: bruskiwang@outlook.com
+    @file: app.py
+    @time: 2020/2/10
+    @desc: 
+"""
+from todoism import create_app
+
+app = create_app('production')
